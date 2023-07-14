fabc958b3d7ecbd1370a46c3d4783eb47d02fa9a
sidx255 <siddharth190820@gmail.com>
2023-07-14 13:29:07 +0530

    chore: variables

---
+++ concepts/variables/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    } 
+    println!("The value of x is: {x}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("The value of five_hundred is: {five_hundred}, six_point_four is: {six_point_four}, one is: {one}");
+}
